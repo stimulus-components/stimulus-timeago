2f75c68da646630371f587611731ba5571a6cee2
Marco Roth <marco.roth@intergga.ch>
2020-11-06 00:59:49 +0100

    simplify `load()` action

--- src/index.js
+++ src/index.js
@@ -19,29 +19,23 @@ export default class extends Controller {
   }
 
   load () {
-    const date = this.data.get('datetime')
+    const datetime = this.data.get('datetime')
+    const date = Date.parse(datetime)
     const options = {
       includeSeconds: this.data.has('includeSeconds') || false,
       addSuffix: this.data.has('addSuffix') || false
     }
 
-    let formattedDate
-    try {
-      formattedDate = formatDistanceToNow(Date.parse(date), options)
-    } catch (e) {
-      if (e instanceof RangeError) {
-        formattedDate = date
-        this.isValid = false
-        console.error(
-          `Value given in "data-timeago-datetime" is not a valid date (${date}). Please provide a ISO 8601 compatible datetime string. Displaying given value instead.`
-        )
-      } else {
-        throw e
-      }
+    if (Number.isNaN(date)) {
+      this.isValid = false
+
+      console.error(
+        `Value given in 'data-timeago-datetime' is not a valid date (${datetime}). Please provide a ISO 8601 compatible datetime string. Displaying given value instead.`
+      )
     }
 
-    this.element.dateTime = date
-    this.element.innerHTML = formattedDate
+    this.element.dateTime = datetime
+    this.element.innerHTML = this.isValid ? formatDistanceToNow(date, options) : datetime
   }
 
   startRefreshing () {
