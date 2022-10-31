500342e18165b339e3b588c5cedc2e72a1ed2288
Guillaume Briday <guillaumebriday@gmail.com>
2022-10-31 14:28:26 +0100

    Config and bump dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Updating workflows
 - Updating Node to 16
 - Bump dependencies
+- Fixing external libraries
 
 ## [4.0.0] - 2021-09-25
 
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-const path = require('path')
+import path from 'path'
 
-module.exports = {
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'stimulus-timeago'
-    },
-    rollupOptions: {
-      external: ['dateFns', 'stimulus'],
-      output: {
-        globals: {
-          stimulus: 'Stimulus',
-          dateFns: 'dateFns'
+export default ({ mode }) => {
+  if (mode === 'netlify') {
+    return {}
+  }
+
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'stimulus-timeago'
+      },
+      rollupOptions: {
+        external: ['date-fns', '@hotwired/stimulus'],
+        output: {
+          globals: {
+            '@hotwired/stimulus': 'Stimulus',
+            'date-fns': 'dateFns'
+          }
         }
       }
     }
